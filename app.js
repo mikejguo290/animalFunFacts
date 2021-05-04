0884fb77bd92c2972253773c06801ae526e44551
mikejguo290 <mikejguo290@gmail.com>
2021-05-04 15:18:04 +0100

    6. reformat the jsx expression in animalFacts to include the bacground variable.

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 const title='';
+
+//can call one JSX element in another by using curly braces. 
 const background=<img className="background" alt="ocean" src="/images/ocean.jpg"/>
-const animalFacts=<h1>{title===''? 'Click an animal for a fun fact.': title }</h1> // hold the JSX expression to contain the title;
+//hold the JSX expression to contain the title;
+const animalFacts=(
+    <div>
+        <h1>{title===''? 'Click an animal for a fun fact.': title }</h1> 
+        {background} 
+    </div>
+)
 
 ReactDOM.render(
     animalFacts,
