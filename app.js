57126356e2629ca1d1864e7564cab7461ff108c1
mikejguo290 <mikejguo290@gmail.com>
2021-05-04 14:56:26 +0100

    2. add title and animalFacts constants.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 import { animals } from './animals';
 import React from 'react';
 import ReactDOM from 'react-dom'
+
+const title='';
+const animalFacts=<h1></h1> // hold the JSX expression to contain the title;
