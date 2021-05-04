98d41ebdf332cd68a3461b392b88ad18e9175d54
mikejguo290 <mikejguo290@gmail.com>
2021-05-04 15:09:40 +0100

    5. added background - an image jsx element.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 const title='';
+const background=<img className="background" alt="ocean" src="/images/ocean.jpg"/>
 const animalFacts=<h1>{title===''? 'Click an animal for a fun fact.': title }</h1> // hold the JSX expression to contain the title;
 
 ReactDOM.render(
