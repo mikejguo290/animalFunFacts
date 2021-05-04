279d4b866873bfcf1b201fcceb83383a700f039e
mikejguo290 <mikejguo290@gmail.com>
2021-05-04 14:59:51 +0100

    add a ternary operator to decide which string to use on the h1 JSX expression.

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 const title='';
-const animalFacts=<h1></h1> // hold the JSX expression to contain the title;
+const animalFacts=<h1>{title===false? 'Click an animal for a fun fact.': title }</h1> // hold the JSX expression to contain the title;
