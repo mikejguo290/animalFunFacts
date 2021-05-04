287efd475f48bc7d9deaeb7afb7bd0eb479eecbe
mikejguo290 <mikejguo290@gmail.com>
2021-05-04 15:53:02 +0100

    7. loop over the animals object imported and push <img /> elements to a new array called inmages.

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ const title='';
 
 //can call one JSX element in another by using curly braces. 
 const background=<img className="background" alt="ocean" src="/images/ocean.jpg"/>
+
+// for each animal in animals imported above, add a new <img /> to that array. 
+const images = [];
+// pseudocode is - for key in obj
+for (let animal in animals){
+  images.push( 
+      (<img 
+        key={animal}
+        className='animal'
+        alt={animal}
+        src={animals[animal].image}
+        aria-label={animal}
+        role='button'
+        />
+        )
+  );
+}
+
 //hold the JSX expression to contain the title;
 const animalFacts=(
     <div>
